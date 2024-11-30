f3a7ed7bfd61594463e7a5347125ef10250d2b0e
LucasCastilho7 <figueiredo.lucas779@gmail.com>
2024-11-30 07:21:51 -0300

    wip: notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:todo_list_app/service/notification_service.dart';
 import 'widget_tree.dart';
 
 void main() async {
   sqfliteFfiInit();
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MainApp());
+
+  final notificationService = NotificationService();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider(create: (context) => notificationService),
+      ],
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
